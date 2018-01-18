cb2b296d671e6bddaf1d5eac4406dcb9b30de9fe
Luis Pestana <33699276+theluispestana@users.noreply.github.com>
2018-01-18 14:18:24 -0500

    adding all files

---
+++ header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+  </head>
+  <body>
+
+    <header>
+        <nav>
+          <div class="main-wrapper">
+            <ul>
+              <li><a href="index.php">Home</a></li>
+            </ul>
+            <div class="nav-login">
+              <form class="" action="index.html" method="post">
+                <input type="text" name="uid"  placeholder="Username/e-mail" value="">
+                <input type="password" name="pwd"  placeholder="password" value="">
+                <button type="submit" name="submit">Login</button>
+              </form>
+              <a href="signup.php">Sign Up</a>
+            </div>
+          </div>
+        </nav>
+    </header>
---
+++ signup.php
@@ -0,0 +1,21 @@
+<?php
+  include_once 'header.php';
+ ?>
+
+<section class="main-container">
+  <div class="main-wrapper">
+    <h2>Sign Up</h2>
+    <form class="signup-form" action="index.html" method="post">
+      <input type="text" name="first" placeholder="First Name" value="">
+      <input type="text" name="last" placeholder="Last Name" value="">
+      <input type="text" name="email" placeholder="E-mail" value="">
+      <input type="text" name="uid" placeholder="Username" value="">
+      <input type="text" name="pwd" placeholder="Password" value="">
+      <button type="submit"name="submit">Sign Up</button>
+    </form>
+  </div>
+</section>
+
+<?php
+  include_once 'footer.php';
+ ?>
